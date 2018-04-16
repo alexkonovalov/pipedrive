d73d68d7623b41b2a226c095f72a26724d370186
alexkonovalov <alvkonovalov@gmail.com>
2018-04-16 03:19:50 +0300

    cleanup layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -19,39 +19,21 @@ import { CardsContainer } from "./smart/cards";
 
 export default class Layout extends React.Component<Pick<any, never>> {
 
-  state : { title : string, collapsed: boolean };
-
-  constructor() {
-    super({});
-     this.state = {
-      title: "Welcome",
-      collapsed: true
-    };
-  }
-
-  changeTitle(title: string) {
-    this.setState({title});
-  }
-
-  toggleNavbar() {
-    this.state.collapsed = !this.state.collapsed;
-  }
-
   render() {
     return (
-      [<Navbar color="dark" inverse expand="md" key="1">
-          <NavbarBrand href="/" className="mr-auto">pipedrive</NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar.bind(this)} className="mr-2" />
-          <Collapse isOpen={false} navbar>
-            <Nav navbar>
-              <NavItem>
-                <NavLink href="https://github.com/alexkonovalov">Github</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-      </Navbar>,
-      <CardsContainer>
-      </CardsContainer>
+      [
+        <Navbar color="dark" dark expand="md" key="nav">
+            <NavbarBrand href="/" className="mr-auto">pipedrive</NavbarBrand>
+            <Collapse isOpen={false} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/alexkonovalov">Github</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+        </Navbar>,
+        <CardsContainer key="cont">
+        </CardsContainer>
       ]
     );
   }
